6198a6906b2bdaaa5682a6c7d5d8e3a340630220
hgcassiopeia <ph.thitikarn_st@tni.ac.th>
2020-12-14 11:12:20 +0700

    edit state

--- js/firstPage.js
+++ js/firstPage.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 const dbRef = firebase.database().ref();
-const stateRef = dbRef.child('state/start');
+const stateRef = dbRef.child('state');
 
 var app = new Vue({
   el: '#app',
@@ -25,7 +25,8 @@ var app = new Vue({
   methods: {
     eventListenerStart() {
       stateRef.on("child_changed", snap => {
-          this.onStart = snap.val()
+          let state = snap.val()
+          this.onStart = state.start
           if(this.onStart) this.goToStart()
       })
     },
