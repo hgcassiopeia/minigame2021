698b922ecf9483dae61308fac8a5f5e2e9ab56dc
hgcassiopeia <ph.thitikarn_st@tni.ac.th>
2020-12-16 17:47:53 +0700

    update

--- js/dashboard.js
+++ js/dashboard.js
@@ -39,7 +39,7 @@ var app = new Vue({
                     displayName: game.displayName,
                     timeScore: game.timeScore,
                     moveScore: game.moveScore,
-                    secondRound: false
+                    secondRound: null
                 }
                 this.gameList.push(tmp)
                 this.gameList = this.gameList.sort((a, b) => a.timeScore - b.timeScore || a.moveScore - b.moveScore)
